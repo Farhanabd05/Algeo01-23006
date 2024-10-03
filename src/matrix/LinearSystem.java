3c14a08a1eada6dd2d5a00fc12f822f05015ab4b
19623248Git <19623248@std.stei.itb.ac.id>
2024-10-04 02:20:59 +0700

    Feat: gauss and gaussJordan function

--- src/matrix/LinearSystem.java
+++ src/matrix/LinearSystem.java
@@ -1,8 +1,10 @@
 package matrix;
 
 public class LinearSystem extends Matrix {
+    
     private Matrix features; // A in Ax=b
     private Matrix target; // b in Ax=b
+
     public LinearSystem(Matrix matrix) {
         super(matrix.getRows(), matrix.getCols());
         features = new Matrix(getRows(), getCols()-1 );
@@ -16,15 +18,35 @@ public class LinearSystem extends Matrix {
     public Matrix getFeatures() {
         return features;
     }
+    
     public Matrix getTarget() {
         return target;
     }
+    
     public void printFeatures() {
         features.displayMatrix();
     }
+
     public void printTarget() {
         target.displayMatrix();
     }
+
+    public double[] gauss(Matrix features, Matrix target){
+        features = getFeatures();
+        target = getTarget();
+        Matrix gaussMatrix = augmentedMatrix(features, target);
+        gaussMatrix.toRowEchelonForm();
+        return backwardSubstitution(gaussMatrix.getData(), target.getCol(0));
+    }
+
+    public double[] gaussJordan(Matrix features, Matrix target){
+        features = getFeatures();
+        target = getTarget();
+        Matrix gaussJordanMatrix = augmentedMatrix(features, target);
+        gaussJordanMatrix.toReducedRowEchelonForm();
+        return gaussJordanMatrix.getCol(gaussJordanMatrix.getCols()-1);
+    }
+
     public Matrix CramerRule() {
         Matrix result = new Matrix(features.getRows(), 1);
         for (int i = 0; i < features.getRows(); i++) {
